3eaa123652bf138af59313f103875bc9b94df221
panth <panth.solanki@gmail.com>
2018-12-25 00:05:04 +0530

    avatar added in Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -14,6 +14,14 @@
 		</v-toolbar>
 
 		<v-navigation-drawer app v-model="drawer" class="primary">
+			<v-layout column align-center>
+				<v-flex class="mt-5">
+					<v-avatar size="100">
+						<img src="/avatar-2.png" alt="">
+					</v-avatar>
+					<p center class="white--text subheading mt-1">Panth</p>
+				</v-flex>
+			</v-layout>
 			<v-list>
 				<v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
 					<v-list-tile-action>
