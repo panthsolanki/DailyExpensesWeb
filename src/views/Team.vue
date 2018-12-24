547873f857f2c0cc7d3c464d5f0d95c8754828f3
panth <panth.solanki@gmail.com>
2018-12-25 00:03:52 +0530

    Team UI created

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -2,13 +2,42 @@
   <div class="tean">
     <h1 class="subheading grey--text">Team</h1>
     <v-container class="my-5">
-      content
+      <v-layout row wrap>
+        <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
+          <v-card flat class="text-xs-center ma-3">
+            <v-responsive class="pt-4">
+              <v-avatar size="100" class="grey ligthen-2">
+                <img :src="person.avatar" alt="avatar">
+              </v-avatar>
+            </v-responsive>
+            <v-card-text>
+              <div class="subheading">{{ person.name }}</div>
+              <div class="grey--text">{{ person.role }}</div>
+            </v-card-text>
+            <v-card-action>
+              <v-btn flat color="grey">
+                <v-icon small left>message</v-icon>
+                <span center>Message</span>
+              </v-btn>
+            </v-card-action>
+          </v-card>
+        </v-flex>
+      </v-layout>
     </v-container>
   </div>
 </template>
 
 <script>
   export default {
-    
+    data() {
+      return {
+        team: [
+          { name: 'ABC', role: 'Web developer', avatar: '/avatar-1.png'},
+          { name: 'adva', role: 'Graphic designer', avatar: '/avatar-2.png'},
+          { name: 'mnas', role: 'Web developer', avatar: '/avatar-3.png'},
+          { name: 'XYS', role: 'Sales guru', avatar:'/avatar-4.png'},
+        ]
+      }
+    }
   }
 </script>
