7d8444a5c4d1cb11671fa3625f9442a20d2ce45c
panth <panth.solanki@gmail.com>
2018-12-30 12:17:51 +0530

    addExpense added in router

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Dashboard from './views/Dashboard.vue'
 import Projects from './views/Projects.vue'
 import Team from './views/Team.vue'
+import AddExpense from './views/AddExpense.vue'
 
 Vue.use(Router)
 
@@ -24,6 +25,11 @@ export default new Router({
       path: '/team',
       name: 'team',
       component: Team
+    },
+    {
+      path: '/add',
+      name: 'addExpense',
+      component: AddExpense
     }
   ]
 })
