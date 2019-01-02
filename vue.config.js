8308752b6c3d55268f98bc0763a10cc52faa4f07
panth <panth.solanki@gmail.com>
2019-01-03 00:07:05 +0530

    Revert "Update vue.config.js"
    This reverts commit e582c5c2d12d6332d0a0be221d4c8f233bad93d5.

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,5 @@
 module.exports = {
   baseUrl: process.env.NODE_ENV === 'production'
     ? '/DailyExpensesWeb/'
-    : '/',
-  pages: {
-    index: {
-      entry: 'dist/index.html',
-      template: 'dist/index.html',
-      filename: 'index.html',
-      title: 'Index Page'
-    }
-  }
+    : '/'
 }
