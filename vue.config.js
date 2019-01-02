e582c5c2d12d6332d0a0be221d4c8f233bad93d5
panth <panth.solanki@gmail.com>
2019-01-02 23:35:13 +0530

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   baseUrl: process.env.NODE_ENV === 'production'
     ? '/DailyExpensesWeb/'
-    : '/'
+    : '/',
+  pages: {
+    index: {
+      entry: 'dist/index.html',
+      template: 'dist/index.html',
+      filename: 'index.html',
+      title: 'Index Page'
+    }
+  }
 }
