4b6d95fc79ada9e3330e19a8b14fcb80dd02e737
Kuliza-325 <you@example.com>
2019-01-08 19:39:51 +0530

    similar date combine in expansion panel

--- src/views/ListExpense.vue
+++ src/views/ListExpense.vue
@@ -2,15 +2,36 @@
   <div class="expenses">
     <h1 class="subheading grey--text">Expenses</h1>
     <v-container class="my-5">
-      <v-expansion-panel>
-        <v-expansion-panel-content v-for="expense in myexpenses" :key="expense.id">
-          <div slot="header">{{expense.date}}</div>
-        <v-card>
-          <v-card-text class="px-4 grey--text">
-            <div class="font-weight-bold">due by {{expense.title}}</div>
-            <div>{{expense.type}}</div>
-          </v-card-text>
-        </v-card>
+      <v-expansion-panel v-for="ed in myexpenses" :key="ed.id">
+        <v-expansion-panel-content>
+          <div slot="header">{{ed.details[0].date}} 
+            <div class="right">
+              <v-chip small class="total white--text caption my-1">{{ed.totalamount}}</v-chip>
+            </div>
+          </div>
+          <v-card>
+            <v-card-text>
+              <v-layout row wrap v-for="expense in ed.details" :key="expense.id" :class="`px-4 pa-3 project ${expense.type}`">
+                <v-flex xs12 md3>
+                  <div>{{expense.type}}</div>
+                  <div class="caption grey--text">{{expense.subtype}}</div>
+                </v-flex>
+                <v-flex xs6 sm4 md5>
+                  <div>{{expense.title}}</div>
+                </v-flex>
+                <v-flex xs6 sm4 md1>
+                  <div class="caption grey--text">Subtype</div>
+                  <div>{{expense.subtype}}</div>
+                </v-flex>
+                <v-flex xs6 sm4 md3>
+                  <div class="right">
+                    <v-chip small :class="`${expense.type} white--text caption my-2`">{{expense.amount}}</v-chip>
+                  </div>
+                </v-flex>
+              </v-layout>
+            </v-card-text>
+            <v-divider></v-divider>
+          </v-card>
         </v-expansion-panel-content>
       </v-expansion-panel>
     </v-container>
@@ -25,22 +46,29 @@ export default {
     return{
       projects: [],
       expenses: [],
-      dates: []
+      dates: [],
+      expensesDatewise: {}
     }
   },
-  computed: {
-    myProjects(){
-      return this.projects.filter(project => {
-        return project.person === 'Panth Solanki'
+  methods: {
+    calmyexpenses(){
+      this.expenses.forEach(expense => {
+        if(!this.expensesDatewise[expense.date]){
+          this.expensesDatewise[expense.date] = {};
+          this.expensesDatewise[expense.date].details = [];
+          this.expensesDatewise[expense.date].totalamount = 0;
+        }
+        
+        this.expensesDatewise[expense.date].details.push({
+          ...expense
+        })
+        this.expensesDatewise[expense.date].totalamount += +expense.amount
       })
+      return this.expensesDatewise
     },
-    myexpenses(){
-      console.log(this.expenses);
-      return this.expenses
-    },
-    getAlldates(){
-      db.collection('expenses').orderBy('date', '').onSnapshot(res => {
-      const changes = res.docChanges();
+    getData(){
+      db.collection('expenses').orderBy('date', 'asc').onSnapshot(res => {
+        const changes = res.docChanges();
 
         changes.forEach(change => {
           if (change.type === 'added'){
@@ -54,21 +82,43 @@ export default {
       })
     }
   },
+  computed: {
+    myProjects(){
+      return this.projects.filter(project => {
+        return project.person === 'Panth Solanki'
+      })
+    },
+    myexpenses() {
+      return this.calmyexpenses()
+    }
+  },
   created() {
     console.log(_.isEmpty() ? 'Lodash is available here!' : 'Uh oh..');
-    db.collection('expenses').onSnapshot(res => {
-      const changes = res.docChanges();
-
-      changes.forEach(change => {
-        if (change.type === 'added'){
-          if(this.expenses)
-          this.expenses.push({
-            ...change.doc.data(),
-            id: change.doc.id
-          })
-        }
-      })
-    })
+    this.getData()
   }
 }
 </script>
+
+<style>
+  .project.food{
+    border-left: 4px solid #3cd1c2;
+  }
+  .project.transport{
+    border-left: 4px solid orange;
+  }
+  .project.monthly{
+    border-left: 4px solid tomato;
+  }
+  .v-chip.food{
+    background: #3cd1c2;
+  }
+  .v-chip.transport{
+    background: orange;
+  }
+  .v-chip.monthly{
+    background: tomato;
+  }
+  .v-chip.total{
+    background: rgb(62, 43, 228);
+  }
+</style>
